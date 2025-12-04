ed9166131011977d4a705dd1741dec851c512d56
raviraj <neoravikumar@gmail.com>
2025-12-04 22:30:43 +0530

    Initial commit

---
+++ src/main/java/com/mystore/onlineproductsales/OnlineProductSalesApplication.java
@@ -0,0 +1,13 @@
+package com.mystore.onlineproductsales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineProductSalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineProductSalesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mystore/online_product_sales/OnlineProductSalesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mystore.online_product_sales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineProductSalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
